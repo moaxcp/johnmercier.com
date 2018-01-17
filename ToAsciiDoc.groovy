a4b69e84883d523fe30b52175eead725e8aafaea
John Mercier <moaxcp@gmail.com>
2018-01-16 21:40:28 -0500

    Finishing conversion and adding post.

--- ToAsciiDoc.groovy
+++ ToAsciiDoc.groovy
@@ -1,36 +1,16 @@
 @Grab('org.yaml:snakeyaml:1.19')
-@Grab('com.atlassian.commonmark:commonmark:0.10.0')
-@Grab('com.atlassian.commonmark:commonmark-ext-yaml-front-matter:0.10.0')
 @Grab('nl.jworks.markdown_to_asciidoc:markdown_to_asciidoc:1.0')
 
 
 import org.yaml.snakeyaml.Yaml
 import nl.jworks.markdown_to_asciidoc.Converter
-import org.commonmark.parser.Parser
-import org.commonmark.renderer.text.TextContentRenderer
-import org.commonmark.node.*
-import org.commonmark.ext.front.matter.*
-
-
-commonmark = Parser.builder()
-        .extensions([YamlFrontMatterExtension.create()])
-        .build()
 
 def source = new File('src/jekyll/_posts')
 def destination = new File('src/jbake/content/blog')
+
 source.eachFile { file ->
     Date fileNameDate = Date.parse('yyyy-MM-dd', file.name.substring(0, 10))
-    String fileNameTitle = file.name.substring(11, file.name.lastIndexOf('.'))
-
-    Document document = commonmark.parse(file.text)
-    Map data = getHeaderAndUnlink(document)
-    convertHeadings(document)
-    String markdown = renderDocument(document)
-    println markdown
-
-
-
-
+    assert fileNameDate
 
     println file
     assert file.text.startsWith('---\n'), file.name
@@ -38,16 +18,10 @@ source.eachFile { file ->
     assert removeStart.contains('---\n'), file.name
     def headerEnd = removeStart.indexOf('---\n')
     def headerText = removeStart.substring(0, headerEnd)
-
     assert headerText
 
     def content = removeStart.substring(headerEnd + 4)
-
     assert content
-
-
-
-    assert fileNameDate
 
     Yaml parser = new Yaml()
     Map header = parser.load(headerText)
@@ -86,51 +60,4 @@ source.eachFile { file ->
         content = content.replaceAll('(?m)^#\\s', '## ')
         it.write Converter.convertMarkdownToAsciiDoc(content)
     }
-}
-
-Map getHeaderAndUnlink(Document document) {
-    def visitor = new AbstractVisitor() {
-        YamlFrontMatterVisitor yamlVisitor = new YamlFrontMatterVisitor()
-        void visit(CustomNode node) {
-            yamlVisitor.visit(node)
-            if(node.class == YamlFrontMatterNode) {
-                node.unlink()
-            }
-            visitChildren(node)
-        }
-        Map<String, List<String>> getData() {
-            return yamlVisitor.data
-        }
-    }
-    document.accept(visitor)
-    assert visitor.data
-    assert visitor.data.keySet()*.toString().every {
-        it in [
-                'layout',
-                'title',
-                'date',
-                'categories',
-                'tags',
-                'status',
-                'type',
-                'published',
-                'meta',
-                'author',
-                'comments'
-        ]
-    }
-    return visitor.data
-}
-
-void convertHeadings(Document document) {
-    document.accept(new AbstractVisitor() {
-        void visit(Heading heading) {
-            heading.level += 1
-            visitChildren(heading)
-        }
-    })
-}
-
-String renderDocument(Document document) {
-    TextContentRenderer.builder().build().render(document)
 }
